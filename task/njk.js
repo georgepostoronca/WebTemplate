9aadf7b83c9651324e78b9e91b40483114defe91
Your Name <you@example.com>
2017-08-04 17:34:22 +0300

    Update HTML Task and .gitignore

--- task/njk.js
+++ task/njk.js
@@ -1,27 +1,28 @@
 module.exports = function (gulp, plugins) {
-    return function () {
-        var time = new Date().getTime();
+  return function () {
+    var time = new Date().getTime()
 
-        gulp.src(['app/njk/*.njk', 'app/block/*.njk'])
-            .pipe(plugins.plumber())
-            .pipe(plugins.nunjucksRender({
-                path: ['./app/njk/', './app/block/'],
-                envOptions: {
-                    trimBlocks: true,
-                    lstripBlocks: true
-                },
-                data: {
-                    timestamp: time
-                }
-            })).on('error', function(err) {
-    			plugins.notify().write(err);
-    			this.emit('end');
-    		})
-            .pipe(plugins.htmlPrettify({
-                indent_size : 4,
-            }))
-            .pipe(plugins.notify({ message: 'HTML task complete' }))
-            .pipe(gulp.dest('app/src/'))
-            .pipe(plugins.browserSync.reload({stream:true}));
-    };
-};
+    gulp.src(['app/njk/*.njk', 'app/block/*.njk'])
+      .pipe(plugins.plumber())
+      .pipe(plugins.nunjucksRender({
+        path: ['./app/njk/', './app/block/'],
+        envOptions: {
+          trimBlocks: true,
+          lstripBlocks: true
+        },
+        data: {
+          timestamp: time
+        }
+      })).on('error', function (err) {
+      plugins.notify().write(err)
+      this.emit('end')
+    })
+      .pipe(plugins.htmlmin({collapseWhitespace: true}))
+      .pipe(plugins.htmlPrettify({
+        indent_size: 4
+      }))
+      .pipe(plugins.notify({ message: 'HTML task complete' }))
+      .pipe(gulp.dest('app/src/'))
+      .pipe(plugins.browserSync.reload({stream: true}))
+  }
+}
