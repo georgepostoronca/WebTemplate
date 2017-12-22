df3968ddfd5ec0427f5e4bf78e68eaf406cb886c
George <george.postoronca@gmail.com>
2017-12-22 11:49:39 +0200

    Add Block and Create Bash Script

---
+++ app/block/block.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Nested if statements
+
+mkdir ./block-$1
+touch ./block-$1/$1.js
+touch ./block-$1/$1.njk
+touch ./block-$1/_$1.scss
---
+++ app/block/create.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Nested if statements
+
+mkdir ./$1
+touch ./$1/$1.js
+touch ./$1/$1.njk
+touch ./$1/_$1.scss
