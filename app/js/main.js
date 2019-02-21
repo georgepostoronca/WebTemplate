8cd9d874e3ba57d49a95bc8e779691b417013ecb
George <george.postoronca@gmail.com>
2019-02-21 16:12:17 +0200

    fix(app file)

--- app/js/main.js
+++ app/js/main.js
@@ -1,21 +1,19 @@
-// include ../block/**/*.js
-
-// import '../block/*.js';
-
-import "../block/header/header.js";
-
 import $ from 'jquery';
 import slick from 'slick-carousel';
 import fancybox from "@fancyapps/fancybox";
 import PerfectScrollbar from 'perfect-scrollbar';
 
+// SVG
+import svg4everybody from "svg4everybody";
+svg4everybody();
 
+// Scrolll
 try {
 	const ps = new PerfectScrollbar('#container');
 } catch (e) {
 	console.log(e);
-
 }
 
-
+// Slick
 $(".carousel").slick();
+
